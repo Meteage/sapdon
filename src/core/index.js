f279076b265e49040822618772ba9180b23301ad
Meteage <3591695712@qq.com>
2025-02-17 17:50:24 +0800

    发布至npm

--- src/core/index.js
+++ src/core/index.js
@@ -1,1 +1,35 @@
-// TODO: 暴露给用户的API
+import { BlockComponent } from "./block/blockComponent.js";
+import { BlockAPI } from "./factory/BlockFactory.js";
+import { EntityAPI } from "./factory/EntityFactory.js";
+import { FeatureAPI } from "./factory/FeatureFactory.js";
+import { ItemAPI } from "./factory/ItemFactory.js";
+import { RecipeAPI } from "./factory/RecipeFactory.js";
+import { ItemComponent } from "./item/ItemComponents.js";
+import { FlipbookTextures, ItemTextureManager, terrainTextureManager } from "./texture.js";
+import { DataBindingObject } from "./ui/DataBindingObject.js";
+import { Image } from "./ui/elements/Image.js";
+import { Label } from "./ui/elements/Label.js";
+import { Panel } from "./ui/elements/Panel.js";
+import { UIElement } from "./ui/elements/UIElement.js";
+import { ServerUISystem } from "./ui/registry/ServerUISystem.js";
+import { UISystem } from "./ui/systems/UISystem.js";
+
+export {
+    ItemAPI,
+    BlockAPI,
+    FeatureAPI,
+    EntityAPI,
+    RecipeAPI,
+    BlockComponent,
+    ItemComponent,
+    Image,
+    Panel,
+    Label,
+    UIElement,
+    UISystem,
+    DataBindingObject,
+    ServerUISystem,
+    ItemTextureManager,
+    terrainTextureManager,
+    FlipbookTextures,
+}
