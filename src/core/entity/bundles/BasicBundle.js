3fa7c343e5fac25c3914defe059d8538a6c48d7d
Meteage <3591695712@qq.com>
2025-03-05 22:38:50 +0800

    Create BasicBundle.js

---
+++ src/core/entity/bundles/BasicBundle.js
@@ -0,0 +1,51 @@
+import { EntityComponent } from "../componets/EntityComponet.js";
+
+export class BasicBundle {
+    //私有属性 存储组件
+    #components = new Map();
+    /** 
+     * 捆绑包 捆绑包是一个附带有一些实体组件的集合，能够方便地将这些组件添加到一个实体上。
+     */
+    constructor() {
+        //空构造函数
+      
+    }
+    /**
+     * 添加组件到捆绑包
+     * @param {Map} componentMap 组件
+     * @returns {BasicBundle} 返回捆绑包对象
+     */
+    addComponent(componentMap) {
+      if (!(componentMap instanceof Map)) {
+          throw new Error("componentMap must be an instance of Map.");
+      }
+
+      for (const [key, value] of componentMap) {
+          this.#components.set(key, value);
+      }
+      return this; // 支持链式调用
+    }
+    addComponents(components){
+      components.forEach(component_map => {
+        this.addComponent(component_map)
+      });
+    }
+    removeComponent(name) {
+        this.#components.delete(name);
+        return this;
+    }
+    //序列化
+    serialize() {
+        return this.#components;
+    }
+}
+
+
+//移动捆绑包
+export const BasicMovementBundle = new BasicBundle();
+
+MovementBundle.addComponents([
+  //speed
+  EntityComponent.setMovement(0.2),
+  EntityComponent.setMovementBasic()
+])
